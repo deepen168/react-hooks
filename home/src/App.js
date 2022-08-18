330ed13c9ae52d79d885de77f18e8904baf727b4
Deepan Patel <deepankumar.patel1@ge.com>
2022-08-17 18:14:51 -0700

    Gallary component working

--- home/src/App.js
+++ home/src/App.js
@@ -1,40 +1,51 @@
-import { useState } from 'react';
-import { Gallary } from './Gallery';
-import { Joke } from './Joke';
-import { Stories } from './Stories';
-import { Tasks } from './Tasks';
+import { useState } from "react";
+import { Gallery } from "./Gallery";
+import { Joke } from "./Joke";
+import { Stories } from "./Stories";
+import { Tasks } from "./Tasks";
 
 export const App = () => {
+  const [userQuery, setUserQuery] = useState("");
+  const [showGallery, setSHowGallery] = useState(true);
 
-    const [userQuery, setUserQuery] = useState('');
+  const updateUserQuery = (e) => setUserQuery(e.target.value);
 
-    const updateUserQuery = (e) => setUserQuery(e.target.value);
-
-    const handleKeyPress = (e) => {
-        if(e.key === 'Enter') {
-            searchQuery();
-        }
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      searchQuery();
     }
+  };
 
-    const searchQuery = () => {
-        window.open(`https://www.google.com/search?q=${userQuery}`, '_blank');
-    }
+  const searchQuery = () => {
+    window.open(`https://www.google.com/search?q=${userQuery}`, "_blank");
+  };
 
-    return (
-        <div>
-            <h1>Hello Deepen!</h1>
-            <div className="form">
-                <input value={userQuery} onChange={updateUserQuery} onKeyPress={handleKeyPress}></input>
-                <button onClick={searchQuery}>Search</button>
-            </div>
-            <hr />
-            <Gallary />
-            <hr />
-            <Tasks />
-            <hr />
-            <Joke />
-            <hr />
-            <Stories />
-        </div>
-    )
-}
+  const toggleGallary = () => {
+    setSHowGallery(!showGallery);
+  };
+
+  return (
+    <div>
+      <h1>Hello Deepen!</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        ></input>
+        <button onClick={searchQuery}>Search</button>
+      </div>
+      <hr />
+      <div>{showGallery ? <Gallery /> : null}</div>
+      <button onClick={toggleGallary}>
+        {showGallery ? "Hide" : "Show"} Gallery
+      </button>
+      <hr />
+      <Tasks />
+      <hr />
+      <Joke />
+      <hr />
+      <Stories />
+    </div>
+  );
+};
--- home/src/Gallery.js
+++ home/src/Gallery.js
@@ -1,14 +1,26 @@
-import photos from './data/pictures';
+import { useEffect, useState } from "react";
+import photos from "./data/pictures";
 
-export const Gallary = () => {
+export const Gallery = () => {
+  const numberOfPhotos = photos.length;
 
-    const numberOfPhotos = photos.length;
+  const [index, setIndex] = useState(0);
 
-    return (
-        <div>
-            {
-                
-            }
-        </div>
-    )
-}
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // setter takes callback to take latest value from useState
+      return setIndex((storedIndex) => (storedIndex + 1) % numberOfPhotos);
+    }, 1000);
+
+    // When you return it runs when component is about to be distroyed
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <img src={photos[index].image} alt="Gallery"></img>
+    </div>
+  );
+};
